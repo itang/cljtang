082e4337f04bb90b9ab5af7e7c04e54f0138bc6e
itang <live.tang@gmail.com>
2013-01-21 16:03:08 +0800

    cleanup

--- project.clj
+++ project.clj
@@ -4,16 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0-RC2"]]
-  :warn-on-reflection true
-  :injections [(require 'clojure.pprint)]
-  :pom-addition [:developers [:developer [:name "itang"]]]
-  :profiles {:user {:dependencies [[org.clojure/clojure "1.5.0-RC2"]]}
-             :dev {:dependencies []
-                   :plugins [[codox "0.6.4"]
+  :profiles {:dev {:plugins [[codox "0.6.4"]
                              [jonase/eastwood "0.0.2"]
                              [lein-localrepo "0.4.1"]
-                             [lein-deps-tree "0.1.2"]]}
+                             [lein-deps-tree "0.1.2"]
+                             [lein-pprint "1.1.1"]]}
              :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0-RC2"]]}}
    :aliases {"run-tests" ["with-profile" "1.4.0:1.5.0" "test"]
-             "slamhound" ["run" "-m" "slam.hound"]})
+             "slamhound" ["run" "-m" "slam.hound"]}
+   :warn-on-reflection true
+   :injections [(require 'clojure.pprint)]
+   :pom-addition [:developers [:developer [:name "itang"]]])

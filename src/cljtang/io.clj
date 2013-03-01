80324023652772f400fba91bce0dbd553da133f5
itang <live.tang@gmail.com>
2013-03-01 15:07:23 +0800

    add cljtang.io

---
+++ src/cljtang/io.clj
@@ -0,0 +1,18 @@
+(ns cljtang.io
+  (:require [clojure.java.io :refer [file]]
+            [clojure.string :refer [join]]))
+
+(defn concatenating
+  "合并文件内容"
+  [src dest & {:keys [separator banner] :or {separator \newline}}]
+  (let [src (if (sequential? src) src [src])
+        src-files (map file src)
+        dest-file (file dest)
+        dest-parent-file (.getParentFile dest-file)]
+    (when-not (.exists dest-parent-file)
+      (.mkdirs dest-parent-file))
+    (spit dest-file (str banner))
+    (doseq [f src-files]
+      (let [separator (if (fn? separator) (separator f) separator)
+            content (slurp f)]
+        (spit dest-file (str separator content) :append true)))))

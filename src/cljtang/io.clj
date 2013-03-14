b3738aff42602791b92841b15a8a196acf9990fd
itang <live.tang@gmail.com>
2013-03-14 21:58:18 +0800

    u

--- src/cljtang/io.clj
+++ src/cljtang/io.clj
@@ -3,15 +3,15 @@
 
 (defn concatenating
   "合并文件内容"
-  [src dest & {:keys [separator banner] :or {separator \newline}}]
-  (let [src (if (sequential? src) src [src])
-        src-files (map file src)
+  [src dest & {:keys [separator banner trace] :or {separator \newline}}]
+  (let [src-files (if (sequential? src) src [src])
         dest-file (file dest)
         dest-parent-file (.getParentFile dest-file)]
     (when-not (.exists dest-parent-file)
       (.mkdirs dest-parent-file))
     (spit dest-file (str banner))
     (doseq [f src-files]
+      (when trace (trace f))
       (let [separator (if (fn? separator) (separator f) separator)
             content (slurp f)]
         (spit dest-file (str separator content) :append true)))))
